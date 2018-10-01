1659ae50ced54dbb69b47e8a071424e1627fb8c4
rajan786 <rajan.gupta@skilrock.com>
2018-10-01 18:08:17 +0530

    listner

--- app/src/main/java/society/doscon/com/doscon/Home.kt
+++ app/src/main/java/society/doscon/com/doscon/Home.kt
@@ -1,22 +1,43 @@
 package society.doscon.com.doscon
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v4.view.PagerAdapter
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.RadioGroup
 import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.home_view.*
 import java.util.*
 
 
-class Home : Fragment() {
+class Home : Fragment(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.abstracttx -> {
+                var intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.eventreg.icegroupindia.com/doscon18/abstract-login.php"))
+                startActivity(intent)
+            }
+            R.id.program -> {
+//                var intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.eventreg.icegroupindia.com/doscon18/abstract-login.php"))
+//                startActivity(intent)
+            }
+            R.id.registration -> {
+                var intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://eventreg.icegroupindia.com/doscon18/"))
+                startActivity(intent)
+            }
+            R.id.notification -> {
+
+            }
+
+        }
+    }
+
     companion object {
         fun newInstance(): Home {
             return Home()
@@ -39,14 +60,30 @@ class Home : Fragment() {
         Handler().postDelayed(Runnable {
             var height: Int = parent_cube.height
             var width: Int = parent_cube.width
-            val params = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT )
-            params.leftMargin=width / 2 - 200
-            params.topMargin=height/2-abstracttx.height/2
+            val params = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+            params.leftMargin = width / 2 - abstracttx.width - 40
+            params.topMargin = height / 2 - abstracttx.height / 2
             abstracttx.layoutParams = params
 
-        },200)
+            val params1 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+            params1.leftMargin = width / 2 + 40
+            params1.topMargin = height / 2 - registration.height / 2
+            registration.layoutParams = params1
 
+            val params2 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+            params2.leftMargin = width / 2 - notification.width / 2
+            params2.topMargin = height / 4
+            notification.layoutParams = params2
 
+            val params3 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+            params3.leftMargin = width / 2 - program.width / 2
+            params3.topMargin = (height / 3) * 2
+            program.layoutParams = params3
+
+        }, 200)
+
+        registration.setOnClickListener(this)
+        abstracttx.setOnClickListener(this)
         var adpter = Carousal(context!!, images)
         carousal.adapter = adpter
         val handler = Handler()
@@ -68,8 +105,9 @@ class Home : Fragment() {
 
     override fun onPause() {
         super.onPause()
-        timer=null
+        timer = null
     }
+
     internal class Carousal(var context: Context, var images: IntArray) : PagerAdapter() {
         lateinit var layoutInflater: LayoutInflater
 
